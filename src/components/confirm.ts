ba49508e070b0ed20a1efed5101293e58da16da4
Facuucabreraa97 <backupfaacuucabrera97@gmail.com>
2025-08-07 18:18:01 -0300

    Delete src/components/confirm.ts

--- src/components/confirm.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Implementa la lógica de confirmación de pago aquí
-  res.status(200).json({ success: true });
-}
